d5ca167fd29708746158c8fd45ce28f51451d4a7
Youwen Wu <youwenw@gmail.com>
2025-04-02 15:57:20 -0700

    feat: upgrade home-manager module

--- flake.nix
+++ flake.nix
@@ -2,25 +2,59 @@
   description = "zen.typ home manager module";
 
   outputs =
-    { self }:
+    { ... }:
     {
       homeManagerModules.default =
         { lib, config, ... }:
         let
           cfg = config.zenTyp;
+          semVer = {
+            major = 0;
+            minor = 5;
+            patch = 0;
+          };
+          generateAllVersions =
+            {
+              major,
+              minor,
+              patch,
+            }:
+            versions:
+            if (minor <= 1) then
+              (generateAllVersions {
+                inherit patch major;
+                minor = minor - 1;
+              })
+              ++ [ "${major}.${minor}.${patch}" ]
+            else
+              [ "${major}.${minor}.${patch}" ];
+
+          versions = generateAllVersions semVer;
+
+          generateTypstToml =
+            version:
+            builtins.toFile "typst.toml" ''
+              [package]
+              name = "zen"
+              version = "${version}"
+              entrypoint = "zen.typ"
+              authors = ["Youwen Wu"]
+              license = "MIT"
+              description = "Blingful Typst template with swag"
+            '';
+          installPath = ".cache/typst/packages/youwen/zen";
+          installPaths = builtins.map (versionStr: {
+            "${installPath}/${versionStr}/zen.typ".source = ./typst/main.typ;
+            "${installPath}/${versionStr}/typst.toml".source = generateTypstToml versionStr;
+            "${installPath}/${versionStr}/template/main.typ".source = ./typst/template/main.typ;
+          }) versions;
         in
         {
           options.zenTyp.enable = lib.mkEnableOption "zen.typ Typst template";
+          options.zenTyp.compat = lib.mkEnableOption "inserting a compatibility shim so all documents with same MAJOR version build";
 
           config = lib.mkIf cfg.enable {
-            home.file.".cache/typst/packages/youwen/zen/0.4.0" = {
-              recursive = true;
-              source = ./typst;
-            };
-            home.file.".cache/typst/packages/youwen/zen/0.2.0" = {
-              recursive = true;
-              source = ./typst;
-            };
+            home.file = installPaths;
           };
         };
     };
